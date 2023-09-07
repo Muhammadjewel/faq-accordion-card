9f3b7c26d040d3d8beae84fea0c3fee45e55ce10
muhammadjewel <msuratov@gmail.com>
2023-09-07 22:28:45 +0500

    Add accordion JS code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-function init () {
+const ACCORDION = {
+  toggler: '.accordion__toggler-button',
+  item: '.accordion__item',
+  itemActive: 'accordion__item--active'
+};
 
+function init () {
+  const elsAccordionTogglerButton = document.querySelectorAll(ACCORDION.toggler);
+
+  elsAccordionTogglerButton.forEach(function (elAccordionTogglerButton) {
+    elAccordionTogglerButton.addEventListener('click', function () {
+      const elAccordionItem = elAccordionTogglerButton.closest(ACCORDION.item);
+      elAccordionItem.classList.toggle(ACCORDION.itemActive);
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
